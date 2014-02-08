59eba05497c636d0ab5a5adb73e7929a6f7a5411
stlk <josef.rousek@gmail.com>
2014-02-08 14:55:17 +0100

    spiked in custom auth backend with persona

--- superlists/settings.py
+++ superlists/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'lists',
     'south',
+    'accounts',
+)
+
+AUTH_USER_MODEL = 'accounts.ListUser'
+AUTHENTICATION_BACKENDS = (
+    'accounts.authentication.PersonaAuthenticationBackend',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -83,3 +89,20 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'DEBUG' if DEBUG else 'INFO'},
+}
